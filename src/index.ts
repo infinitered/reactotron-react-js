88e2115bf4fd9106506e70095d941482ac1c9418
Kevin VanGelder <kevin@infinite.red>
2022-07-21 10:41:46 -0700

    attempted fix to allow use in NextJS projects

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,10 @@ export { trackGlobalErrors }
 
 const REACTOTRON_ASYNC_CLIENT_ID = "@REACTOTRON/clientId"
 
+function isBrowser() {
+  return typeof window !== 'undefined'
+}
+
 /**
  * Safely get some information out the the window.navigator.
  *
@@ -16,35 +20,39 @@ const REACTOTRON_ASYNC_CLIENT_ID = "@REACTOTRON/clientId"
  */
 function getNavigatorProperty(name) {
   if (!name) return undefined
-  if (!window) return undefined
+  if (!isBrowser()) return undefined
   if (!window.navigator && typeof window.navigator !== "object") return undefined
   return window.navigator[name]
 }
 
-const DEFAULTS = {
-  createSocket: path => new WebSocket(path), // eslint-disable-line
-  host: "localhost",
-  port: 9090,
-  name: "React JS App",
-  client: {
-    reactotronLibraryName: "reactotron-react-js",
-    reactotronLibraryVersion: "REACTOTRON_REACT_JS_VERSION",
-    platform: "browser",
-    platformVersion: getNavigatorProperty("platform"),
-    userAgent: getNavigatorProperty("userAgent"),
-    screenWidth: (screen && screen.width) || undefined,
-    screenHeight: (screen && screen.height) || undefined,
-    screenScale: (window && window.devicePixelRatio) || 1,
-    windowWidth: (window && window.innerWidth) || undefined,
-    windowHeight: (window && window.innerHeight) || undefined,
-  },
-  getClientId: () => {
-    return Promise.resolve(localStorage.getItem(REACTOTRON_ASYNC_CLIENT_ID))
-  },
-  setClientId: (clientId: any) => {
-    localStorage.setItem(REACTOTRON_ASYNC_CLIENT_ID, clientId)
-    return Promise.resolve()
-  },
+let DEFAULTS = {};
+
+if (isBrowser()) {
+  DEFAULTS = {
+    createSocket: path => new WebSocket(path), // eslint-disable-line
+    host: "localhost",
+    port: 9090,
+    name: "React JS App",
+    client: {
+      reactotronLibraryName: "reactotron-react-js",
+      reactotronLibraryVersion: "REACTOTRON_REACT_JS_VERSION",
+      platform: "browser",
+      platformVersion: getNavigatorProperty("platform"),
+      userAgent: getNavigatorProperty("userAgent"),
+      screenWidth: (screen && screen.width) || undefined,
+      screenHeight: (screen && screen.height) || undefined,
+      screenScale: (window && window.devicePixelRatio) || 1,
+      windowWidth: (window && window.innerWidth) || undefined,
+      windowHeight: (window && window.innerHeight) || undefined,
+    },
+    getClientId: () => {
+      return Promise.resolve(localStorage.getItem(REACTOTRON_ASYNC_CLIENT_ID))
+    },
+    setClientId: (clientId: any) => {
+      localStorage.setItem(REACTOTRON_ASYNC_CLIENT_ID, clientId)
+      return Promise.resolve()
+    },
+  }
 }
 
 // -----------
